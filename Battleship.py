73eba536680faf5f200841b038b2b9cb9477e8ee
H_Laptop <qhw20230516@outlook.com>
2025-09-10 19:56:47 +0800

    adding new features in lab-wk-7

--- Battleship.py
+++ Battleship.py
@@ -1,7 +1,42 @@
+#Definition of Varibles
 mpSurface:list[list[int]]=[[0 for j in range(10)] for i in range(10)]
 mpBelow: list[list[int]]=[[0 for j in range(10)] for i in range(10)]
 lengthTable={"Carrier":4,"Submarine":3}
 shipTable={"Carrier":6,"Submarine":5}
+
+#Definition of Functions
+
+def placeShip(x,y,depth,ori,type):
+    x=int(x)
+    y=int(y)
+    depth=int(depth)
+    if depth==1:
+        if ori=="vertical":
+            for i in range(0,lengthTable[type]):
+                if mpSurface[x+i][y]==0:
+                    mpSurface[x+i][y]=shipTable[type]
+                else:
+                    return False
+        else:
+            for i in range(0,lengthTable[type]):
+                if mpBelow[x][y+i]==0:
+                    mpSurface[x][y+i]=shipTable[type]
+                else:
+                    return False
+    else:
+        if ori=="vertical":
+            for i in range(0,lengthTable[type]):
+                if mpBelow[x+i][y]==0:
+                    mpBelow[x+i][y]=shipTable[type]
+                else:
+                    return False
+        else:
+            for i in range(0,lengthTable[type]):
+                if mpBelow[x][y+i]==0:
+                    mpBelow[x][y+i]=shipTable[type]
+                else:
+                    return False
+    return True
 
 def isShipValid(x,y,depth,ori,type):
     x=int(x)
@@ -10,10 +45,10 @@ def isShipValid(x,y,depth,ori,type):
     shipLength=lengthTable[type]
 
     if ((type=="Carrier"and depth!=0)or(type=="Submarine"))and((ori=="vertical")and(0<=x+shipLength-1 and x+shipLength-1 <boardsize))or((ori=="horizontal")and(0<=y+shipLength-1 and y+shipLength-1 <boardsize))and(ori=="horizontal" or ori=="vertical")and(0<=x and x<boardsize)and(0<=y and y<boardsize):
-        print("Valid")
-        return True
+        print("Valid Coord...Placing Ship")
+        return placeShip(x,y,depth,ori,type)
     else:
-        print("Invalid")
+        print("Invalid Coord!")
         return False
 
 def isCoordValid(x,y,depth):
@@ -30,34 +65,27 @@ def printMap():
     for i in range(0,10):
         print(mpBelow[i])
 
-def placeShip(x,y,depth,ori,type):
-    x=int(x)
-    y=int(y)
-    depth=int(depth)
-    if depth==1:
-        if ori=="vertical":
-            for i in range(0,lengthTable[type]):
-                mpSurface[x+i][y]=shipTable[type]
-        else:
-            for i in range(0,lengthTable[type]):
-                mpSurface[x][y+i]=shipTable[type]
-    else:
-        if ori=="vertical":
-            for i in range(0,lengthTable[type]):
-                mpBelow[x+i][y]=shipTable[type]
-        else:
-            for i in range(0,lengthTable[type]):
-                mpBelow[x][y+i]=shipTable[type]
 
-print("###PLACING THE SHIPS###")
-x,y,depth,ori,type=input("Please input the coord(line,col,depth),ori,type of the ship:").split()
-while True:
-    if isShipValid(x,y,depth,ori,type):
-        placeShip(x,y,depth,ori,type)
-        break
-    x,y,ori,type=input("Please input the coord(line,col),ori,type of the ship:").split()
-    
-x,y,depth=input("Input the coord(line,col,depth):").split()
+# noinspection PyShadowingNames
+def getInitInput():
+    print("--------========####BATTLESHIP_GAME####========--------")
+    x,y,depth,ori =input('Please enter coord of Carrier (line, col, depth) and orientation(vertical/horizontal): ').split()
+    while True:
+        if isShipValid(x, y, depth, ori,"Carrier"):
+            print("Carrier placed")
+            break
+        x,y,depth,ori = input('Please enter coord of Carrier (line, col, depth) and orientation(vertical/horizontal): ').split()
+    x,y,depth,ori = input('Please enter coord of Carrier (line, col, depth) and orientation(vertical/horizontal): ').split()
+    while True:
+        if isShipValid(x, y, depth, ori, "Submarine"):
+            print("Submarine placed")
+            break
+        x,y,depth,ori = input('Please enter coord of Carrier (line, col, depth) and orientation(vertical/horizontal): ').split()
+
+
+#Main programme starts here
+getInitInput()
+x,y,depth=input("Input the attack coord(line,col,depth):").split()
 x=int(x)
 y=int(y)
 depth=int(depth)
